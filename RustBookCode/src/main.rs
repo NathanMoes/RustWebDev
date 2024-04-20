d8299b7396d1d9dadc51652f6e5f68848f7e0652
NathanMoes <nathanmoes7@gmail.com>
2024-04-20 16:56:10 -0700

    More code

--- RustBookCode/src/main.rs
+++ RustBookCode/src/main.rs
@@ -1,9 +1,8 @@
 use axum::http::header::CONTENT_TYPE;
 use axum::http::HeaderValue;
 use axum::routing::{delete, put};
-use axum::Json;
 use axum::{
-    extract::{Query, State},
+    extract::{Json, Query, State},
     http::{Method, StatusCode},
     response::{IntoResponse, Response},
     routing::get,
@@ -34,6 +33,11 @@ struct Pagination {
     end: Option<QuestionId>,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+struct IdParam {
+    id: Option<String>,
+}
+
 #[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq, Hash)]
 struct QuestionId(String);
 
@@ -52,11 +56,7 @@ async fn post_question(
     State(state): State<AppState>,
     Json(question): Json<Question>,
 ) -> impl IntoResponse {
-    state
-        .questions
-        .write()
-        .await
-        .insert(question.id.clone(), question);
+    state.add_question(question).await;
     Response::builder()
         .status(StatusCode::OK)
         .body("Question added".to_string())
@@ -72,6 +72,7 @@ async fn post_question(
 enum ApiError {
     ParseError(std::num::ParseIntError),
     MissingParameters,
+    QuestionNotFound,
 }
 
 impl IntoResponse for ApiError {
@@ -85,6 +86,10 @@ impl IntoResponse for ApiError {
                 .status(StatusCode::BAD_REQUEST)
                 .body("Missing parameter".into())
                 .unwrap(),
+            ApiError::QuestionNotFound => Response::builder()
+                .status(StatusCode::NOT_FOUND)
+                .body("Question not found".into())
+                .unwrap(),
         }
     }
 }
@@ -94,6 +99,7 @@ impl std::fmt::Display for ApiError {
         match self {
             ApiError::ParseError(e) => write!(f, "Cannot parse parameter: {}", e),
             ApiError::MissingParameters => write!(f, "Missing parameter"),
+            ApiError::QuestionNotFound => write!(f, "Question not found"),
         }
     }
 }
@@ -131,6 +137,16 @@ impl AppState {
             .insert(question.id.clone(), question);
         self
     }
+
+    async fn delete_question(self, id: &QuestionId) -> Self {
+        self.questions.write().await.remove(id);
+        self
+    }
+
+    async fn update_question(self, id: &QuestionId, question: Question) -> Self {
+        self.questions.write().await.insert(id.clone(), question);
+        self
+    }
 }
 
 impl Clone for Question {
@@ -141,6 +157,26 @@ impl Clone for Question {
             content: self.content.clone(),
             tags: self.tags.clone(),
         }
+    }
+}
+
+async fn update_question(
+    State(state): State<AppState>,
+    Json(question): Json<Question>,
+    id: QuestionId,
+) -> impl IntoResponse {
+    match state.get_question(&id).await {
+        Some(_) => {
+            state.update_question(&id, question).await;
+            Response::builder()
+                .status(StatusCode::OK)
+                .body("Question updated".to_string())
+                .unwrap()
+        }
+        None => Response::builder()
+            .status(StatusCode::NOT_FOUND)
+            .body(ApiError::QuestionNotFound.to_string())
+            .unwrap(),
     }
 }
 
@@ -213,6 +249,31 @@ async fn get_questions(
     }
 }
 
+async fn get_question(
+    State(state): State<AppState>,
+    Query(IdParam { id }): Query<IdParam>,
+) -> impl IntoResponse {
+    match id {
+        Some(id) => {
+            let question_id = QuestionId(id);
+            match state.get_question(&question_id).await {
+                Some(question) => Response::builder()
+                    .status(StatusCode::OK)
+                    .body(serde_json::to_string_pretty(&question).unwrap())
+                    .unwrap(),
+                None => Response::builder()
+                    .status(StatusCode::NOT_FOUND)
+                    .body(ApiError::QuestionNotFound.to_string())
+                    .unwrap(),
+            }
+        }
+        None => Response::builder()
+            .status(StatusCode::BAD_REQUEST)
+            .body(ApiError::MissingParameters.to_string())
+            .unwrap(),
+    }
+}
+
 impl FromStr for QuestionId {
     type Err = std::io::Error;
 
@@ -231,6 +292,26 @@ async fn handle_not_found() -> impl IntoResponse {
         .unwrap()
 }
 
+async fn put_question(
+    State(state): State<AppState>,
+    Json(question): Json<Question>,
+) -> impl IntoResponse {
+    let question_id = question.id.clone();
+    match state.get_question(&question_id).await {
+        Some(_) => {
+            state.update_question(&question_id, question).await;
+            Response::builder()
+                .status(StatusCode::OK)
+                .body("Question updated".to_string())
+                .unwrap()
+        }
+        None => Response::builder()
+            .status(StatusCode::NOT_FOUND)
+            .body(ApiError::QuestionNotFound.to_string())
+            .unwrap(),
+    }
+}
+
 #[tokio::main]
 async fn main() {
     let cors = CorsLayer::new()
@@ -242,12 +323,14 @@ async fn main() {
     let state = AppState::new();
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
-        .route("/questions", get(get_questions).with_state(state.clone()))
-        .route("/questions", post(post_question).with_state(state))
-        .route("/questions/:id", put(handle_not_found))
+        .route("/questions", get(get_questions))
+        .route("/questions", post(post_question))
+        .route("/question", get(get_question))
+        .route("/questions/:id", put(put_question))
         .route("/questions/:id", delete(handle_not_found))
         .route("/answers", post(handle_not_found))
         .layer(cors)
+        .with_state(state)
         .fallback(handle_not_found);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
