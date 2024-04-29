c8a62a1957bcca04fd4d4db5c5921b730cd6ded9
NathanMoes <nathanmoes7@gmail.com>
2024-04-29 15:11:30 -0700

    Attr knock knock

--- src/main.rs
+++ src/main.rs
@@ -52,6 +52,7 @@ async fn main() {
         .with(tracing_subscriber::fmt::layer())
         .init();
     // https://carlosmv.hashnode.dev/adding-logging-and-tracing-to-an-axum-app-rust
+    // Credit to course knock-knock for the trace layer
     let trace_layer = trace::TraceLayer::new_for_http()
         .make_span_with(trace::DefaultMakeSpan::new().level(tracing::Level::INFO))
         .on_response(trace::DefaultOnResponse::new().level(tracing::Level::INFO));
